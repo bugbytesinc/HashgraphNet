a0db149d9193d02945c6d3dd407459943227e8d1
Jason Fabritz <msdn@bugbytes.com>
2019-05-01 23:11:08 -0400

    Add Example Website

---
+++ examples/Hashgraph.NetCore.Web/Models/GetBalanceRequestModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hashgraph.Net.Web.Models
+{
+    public class GetBalanceRequestModel
+    {
+        [Required(ErrorMessage = "The Test Network Address is required.")]
+        public string GatewayName { get; set; }
+        [Required(ErrorMessage = "The Test Network Port is required.")]
+        [Range(1, int.MaxValue, ErrorMessage = "A valid Test Network Port is required.")]
+        public int GatewayPort { get; set; }
+        [Required(ErrorMessage = "The Node Realm is Required.")]
+        [Range(0, int.MaxValue, ErrorMessage = "A valid Node Realm value is required.")]
+        public long GatewayRealmNum { get; set; }
+        [Required(ErrorMessage = "The Node Shard is Required.")]
+        [Range(0, int.MaxValue, ErrorMessage = "A valid Node Shard value is required.")]
+        public long GatewayShardNum { get; set; }
+        [Required(ErrorMessage = "The Node Account Number is Required.")]
+        [Range(3, int.MaxValue, ErrorMessage = "A valid Node account number value is required.")]
+        public long GatewayAccountNum { get; set; }
+        [Required(ErrorMessage = "The Payer Realm is Required.")]
+        [Range(0, int.MaxValue, ErrorMessage = "A valid Payer Realm value is required.")]
+        public long PayerRealmNum { get; set; }
+        [Required(ErrorMessage = "The Payer Shard is Required.")]
+        [Range(0, int.MaxValue, ErrorMessage = "A valid Payer Shard value is required.")]
+        public long PayerShardNum { get; set; }
+        [Required(ErrorMessage = "The Payer Account Number is Required.")]
+        [Range(3, int.MaxValue, ErrorMessage = "A valid Payer account number value is required.")]
+        public long PayerAccountNum { get; set; }
+        [Required(ErrorMessage = "The paying account private key is required.")]
+        [DataType(DataType.Password)]
+        public string PayerPrivateKey { get; set; }
+        [Required(ErrorMessage = "The Account Realm is Required.")]
+        [Range(0, int.MaxValue, ErrorMessage = "A valid Account Realm value is required.")]
+        public long AccountRealmNum { get; set; }
+        [Required(ErrorMessage = "The Account Shard is Required.")]
+        [Range(0, int.MaxValue, ErrorMessage = "A valid Account Shard value is required.")]
+        public long AccountShardNum { get; set; }
+        [Required(ErrorMessage = "The Account Number is Required.")]
+        [Range(3, int.MaxValue, ErrorMessage = "A valid account number value is required.")]
+        public long AccountAccountNum { get; set; }
+
+    }
+}
---
+++ examples/Hashgraph.NetCore.Web/Pages/Index.cshtml.cs
@@ -0,0 +1,67 @@
+﻿using Hashgraph.Net.Web.Models;
+using Hashgraph.NetCore;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Threading.Tasks;
+
+namespace Hashgraph.NetCore.Web.Pages
+{
+    public class IndexModel : PageModel
+    {
+        [BindProperty]
+        public GetBalanceRequestModel GetBalanceRequest { get; set; }
+        public ulong? Balance { get; private set; }
+        public string ErrorMessage { get; private set; }
+        public void OnGet()
+        {
+            GetBalanceRequest = new GetBalanceRequestModel
+            {
+                GatewayName = "testnet.hedera.com",
+                GatewayPort = 0,
+                GatewayRealmNum = 0,
+                GatewayShardNum = 0,
+                GatewayAccountNum = 3,
+                PayerRealmNum = 0,
+                PayerShardNum = 0,
+                PayerAccountNum = 3,
+                AccountRealmNum = 0,
+                AccountShardNum = 0,
+                AccountAccountNum = 3
+            };
+        }
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            try
+            {
+                var client = new Client(ctx =>
+                {
+                    ctx.Gateway = new Gateway(
+                        $"{GetBalanceRequest.GatewayName}:{GetBalanceRequest.GatewayPort}",
+                        GetBalanceRequest.GatewayRealmNum,
+                        GetBalanceRequest.GatewayShardNum,
+                        GetBalanceRequest.GatewayAccountNum);
+                    ctx.Payer = new Account(
+                        GetBalanceRequest.PayerRealmNum,
+                        GetBalanceRequest.PayerShardNum,
+                        GetBalanceRequest.PayerAccountNum,
+                        GetBalanceRequest.PayerPrivateKey);
+                });
+                Balance = await client.GetAccountBalanceAsync(
+                    new Address(
+                        GetBalanceRequest.AccountRealmNum,
+                        GetBalanceRequest.AccountShardNum,
+                        GetBalanceRequest.AccountAccountNum));
+            }
+            catch (Exception ex)
+            {
+                ErrorMessage = ex.ToString();
+            }
+            return Page();
+        }
+    }
+}
